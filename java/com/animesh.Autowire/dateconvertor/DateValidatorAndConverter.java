7f8bdcf638dca001b6debe0e44dd66b6ccc18422
animeshnag <nag.animesh56@gmail.com>
2020-03-15 20:22:30 +0530

    First commit

---
+++ java/com/animesh.Autowire/dateconvertor/DateValidatorAndConverter.java
@@ -0,0 +1,32 @@
+package com.animesh;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class DateValidatorAndConverter implements FactoryBean<Date> {
+    @Value("${datePattern}")
+    String datePattern;
+    @Value("${dateInput}")
+    String dateInput;
+
+    @Override
+    public Date getObject() throws ParseException {
+        return new SimpleDateFormat(datePattern).parse(dateInput);
+    }
+    @Override
+    public Class<?> getObjectType()
+    {
+        return java.util.Date.class;
+    }
+    @Override
+    public boolean isSingleton() {
+        return false;
+    }
+}
+
+
+
---
+++ java/com/animesh.Autowire/master/Master.java
@@ -0,0 +1,13 @@
+package com.animesh;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Master {
+    public static void main( String[] args )
+    {
+        ApplicationContext context= new ClassPathXmlApplicationContext("config.xml");
+        Employee employee=(Employee)context.getBean("employee");
+        System.out.println(employee.toString());
+    }
+}
